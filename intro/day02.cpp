be00b5334407c7079141c9a69123692478a66981
shashwat0cdr <shashwatgujrati@gmail.com>
2026-06-25 15:30:56 +0530

    first commit

---
+++ intro/day02.cpp
@@ -0,0 +1,20 @@
+// Variables and Datatypes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaring variables
+    int age = 25;
+    float height = 5.8;
+    char grade = 'A';
+    bool isStudent = true;
+
+    // Printing variable values
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << " foot" << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
---
+++ intro/day_01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Namaste! Duniya :) \n " ;
+    cout << " Hello! World :) \n " ;
+    
+    
+    return 0;
+
+}
